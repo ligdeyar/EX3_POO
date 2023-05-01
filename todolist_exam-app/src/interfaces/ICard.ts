afce96c9f990426f1972022e215efb8f04b33871
ligdeyar <99105563+ligdeyar@users.noreply.github.com>
2023-05-01 13:56:04 -0600

    Delete todolist_exam-app directory

--- todolist_exam-app/src/interfaces/ICard.ts
+++
@@ -1,7 +0,0 @@
-import { ITask } from "./ITask";
-
-export interface ICard {
-    task: ITask;
-    changeStatus: (id: number, status: string) => void;
-    deleteTask: (id: number) => void;
-}
--- todolist_exam-app/src/interfaces/IPanel.ts
+++
@@ -1,8 +0,0 @@
-import { ITask } from "./ITask";
-
-export interface IPanelProps {
-    title: string;
-    tasks: ITask[];
-    changeStatus: (id: number, status: string) => void;
-    deleteTask: (id: number) => void;
-}
